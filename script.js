c8b849a5af4ec40ce23f136cacb426d1c140b154
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-12-17 23:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	 let chunks = [];
+   for(let i=0; i<str.length; i = i + size) {
+      let subStr = str.slice(i, i+size);
+    //   console.log(subStr);
+      chunks.push(subStr);
+   }
+   return chunks;
+}
+
+// console.log(stringChop("12345", 2));
+console.log(stringChop("Hello, world!", 5));
 }
 
 // Do not change the code below
